5b8dae0a9510a0030defb6bd8aa7857f364060e2
Esteban <dantes-_inferno@hotmail.com>
2017-05-13 09:47:58 -0500

    Add files via upload

---
+++ ejercicios2/ejercicios.py
@@ -0,0 +1,39 @@
+n1=2
+n2=9
+n3=6
+max_de_tres(n1)
+max_de_tres(n2)
+max_de_tres(n3) 
+
+def max_de_tres(num):
+
+	if(num > num):
+		print(num)
+
+
+entero = int(input("numero:"))
+caracter = input("caracter:")
+
+def generate_n_caracteres(entero, caracter):
+	res = entero * caracter
+	print(res)
+
+generate_n_caracteres(entero, caracter)
+
+
+
+cantidad = int(input("cuantos numero:"))
+
+for i in range(cantidad):
+	lista = int(input("numero:"))
+	lista_proceso = [lista,+i]
+
+caracter = input("caracter:")
+
+def procedimiento(lista, caracter):
+	for r in lista:
+		res = r * caracter
+		print(res,"\n")
+
+
+procedimiento(lista_proceso, caracter)
